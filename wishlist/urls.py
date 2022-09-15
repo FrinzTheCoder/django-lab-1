9d2fb07005aeebf8681e5a500f20e47b74be3ccf
= <=>
2022-09-15 13:21:00 +0700

    LAB 2 COMMIT

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import func_xml
+from wishlist.views import func_json
+from wishlist.views import show_json_id
+from wishlist.views import show_xml_id
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', func_xml, name='func_xml'),
+    path('json/', func_json, name='func_json'),
+    path('json/<int:id>', show_json_id, name='show_json_id'),
+    path('xml/<int:id>', show_xml_id, name='show_xml_id'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 data_barang_wishlist = BarangWishlist.objects.all()
 context = {
@@ -9,3 +11,20 @@ context = {
 
 def show_wishlist(request):
     return render(request, "wishlist.html", context)
+
+def func_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def func_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")    
+
